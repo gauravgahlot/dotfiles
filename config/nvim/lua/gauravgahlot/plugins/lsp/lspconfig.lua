11e4bcc9c41a73b2dc0f3dca1da4a316d4fc72c1
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-06-01 20:54:55 +0530

    enable virtual text for diagnostics
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- config/nvim/lua/gauravgahlot/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/gauravgahlot/plugins/lsp/lspconfig.lua
@@ -1,37 +1,37 @@
 -- import lspconfig plugin safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-	return
+  return
 end
 
 -- import cmp-nvim-lsp plugin safely
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-	return
+  return
 end
 
 local keymap = vim.keymap -- for conciseness
 
 -- enable keybinds only for when lsp server available
 local on_attach = function(client, bufnr)
-	-- keybind options
-	local opts = { noremap = true, silent = true, buffer = bufnr }
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
 
-	-- set keybinds
-	keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-	keymap.set("n", "[d", vim.diagnostic.goto_prev)
-	keymap.set("n", "]d", vim.diagnostic.goto_next)
-	keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+  -- set keybinds
+  keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+  keymap.set("n", "[d", vim.diagnostic.goto_prev)
+  keymap.set("n", "]d", vim.diagnostic.goto_next)
+  keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
-	keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-	keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
-	keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- go to implementation
-	keymap.set("n", "gr", vim.lsp.buf.references, opts) -- show references
+  keymap.set("n", "gD", vim.lsp.buf.declaration, opts)                -- go to declaration
+  keymap.set("n", "gd", vim.lsp.buf.definition, opts)                 -- go to definition
+  keymap.set("n", "gi", vim.lsp.buf.implementation, opts)             -- go to implementation
+  keymap.set("n", "gr", vim.lsp.buf.references, opts)                 -- show references
 
-	keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under the cursor
-	-- keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts) -- signature help
-	keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts) -- rename what is under cursor
-	keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts) -- show code actions
+  keymap.set("n", "K", vim.lsp.buf.hover, opts)                       -- show documentation for what is under the cursor
+  -- keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts) -- signature help
+  keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)              -- rename what is under cursor
+  keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts) -- show code actions
 end
 
 -- setup rust-tools
@@ -51,98 +51,98 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- configure emmet language server
 lspconfig["rust_analyzer"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	},
-	settings = {
-		["rust-analyzer"] = {
-			cargo = {
-				allFeatures = true,
-			},
-			completion = {
-				postfix = {
-					enable = true,
-				},
-			},
-		},
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      completion = {
+        postfix = {
+          enable = true,
+        },
+      },
+    },
+  },
 })
 
 local lspconf = require("lspconfig")
 lspconf.gopls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	cmd = { "gopls" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	-- root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	settings = {
-		gopls = {
-			gofumpt = true,
-			codelenses = {
-				gc_details = false,
-				generate = true,
-				regenerate_cgo = true,
-				run_govulncheck = true,
-				test = true,
-				tidy = true,
-				upgrade_dependency = true,
-				vendor = true,
-			},
-			hints = {
-				assignVariableTypes = true,
-				compositeLiteralFields = true,
-				compositeLiteralTypes = true,
-				constantValues = true,
-				functionTypeParameters = true,
-				parameterNames = true,
-				rangeVariableTypes = true,
-			},
-			analyses = {
-				fieldalignment = true,
-				nilness = true,
-				unusedparams = true,
-				unusedwrite = true,
-				useany = true,
-			},
-			usePlaceholders = true,
-			completeUnimported = true,
-			staticcheck = true,
-			directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules", "-.nvim" },
-			semanticTokens = true,
-		},
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  -- root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      gofumpt = true,
+      codelenses = {
+        gc_details = false,
+        generate = true,
+        regenerate_cgo = true,
+        run_govulncheck = true,
+        test = true,
+        tidy = true,
+        upgrade_dependency = true,
+        vendor = true,
+      },
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      analyses = {
+        fieldalignment = true,
+        nilness = true,
+        unusedparams = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      usePlaceholders = true,
+      completeUnimported = true,
+      staticcheck = true,
+      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules", "-.nvim" },
+      semanticTokens = true,
+    },
+  },
 })
 
 -- configure lua server (with special settings)
 lspconf["lua_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = { -- custom settings for lua
-		Lua = {
-			-- make the language server recognize "vim" global
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				-- make language server aware of runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = { -- custom settings for lua
+    Lua = {
+      -- make the language server recognize "vim" global
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        -- make language server aware of runtime files
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  },
 })
 
 -- LSP Diagnostics Options Setup
 local sign = function(opts)
-	vim.fn.sign_define(opts.name, {
-		texthl = opts.name,
-		text = opts.text,
-		numhl = "",
-	})
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = "",
+  })
 end
 
 sign({ name = "DiagnosticSignError", text = "" })
@@ -151,17 +151,17 @@ sign({ name = "DiagnosticSignHint", text = "" })
 sign({ name = "DiagnosticSignInfo", text = "" })
 
 vim.diagnostic.config({
-	virtual_text = false,
-	signs = true,
-	update_in_insert = true,
-	underline = true,
-	severity_sort = false,
-	float = {
-		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = false,
+  float = {
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+  },
 })
 
 vim.cmd([[
