ce5009605aa4e847bda6b7a7aa26bf6b42f6ef74
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-11-15 00:06:55 +0100

    I must enjoy torturing myself
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- config/hypr/scripts/external-only.sh
+++ config/hypr/scripts/external-only.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# Turn on display power
 hyprctl dispatch dpms on
 
-# set external monitor as the primary display
-hyprctl keyword monitor "HDMI-A-1,preferred,auto,auto"
+# Get all external (non-eDP) monitors
+monitors=($(hyprctl monitors -j | jq -r '.[] | select(.name | contains("eDP") | not) | .name'))
 
-# disable the internal monitor
+# If at least one external monitor exists
+if [[ ${#monitors[@]} -ge 1 ]]; then
+    # Set the first as normal
+    hyprctl keyword monitor "${monitors[0]},preferred,0x0,1,transform,0"
+fi
+
+# If a second external monitor exists
+if [[ ${#monitors[@]} -ge 2 ]]; then
+    # Rotate second by 270° and position it to the right of the first
+    hyprctl keyword monitor "${monitors[1]},preferred,1920x0,1,transform,3"
+fi
+
+# Disable the laptop's built-in display
 hyprctl keyword monitor "eDP-1,disable"
 
