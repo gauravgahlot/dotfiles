cea1eaffc86f7babf50784930ff634096124e136
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2026-02-08 21:32:12 +0100

    fix: lua config
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- .config/nvim/lua/config/core/keymaps.lua
+++ .config/nvim/lua/config/core/keymaps.lua
@@ -25,21 +25,11 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- plugin keymaps
-
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-
--- telescope
-local builtin = require("telescope.builtin")
--- keymap.set("n", "<leader>ff", builtin.find_files, {})  -- find files within current working directory, respects .gitignore
--- keymap.set("n", "<leader>fs", builtin.live_grep, {})   -- find string in current working directory as you type
--- keymap.set("n", "<leader>fc", builtin.grep_string, {}) -- find string under cursor in current working directory
--- keymap.set("n", "<leader>fb", builtin.buffers, {}) -- list open buffers in current neovim instance
--- keymap.set("n", "<leader>fh", builtin.help_tags, {}) -- list available help tags
 
 -- FloaTerm configuration
 keymap.set("n", "<leader>ft", ":FloatermNew --name=myfloat --height=0.9 --width=0.7 --autoclose=2 zsh<CR>")
