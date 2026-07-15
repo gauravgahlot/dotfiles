da5869cc006928ac55d5f878d134addf7e9d6478
Gaurav Gahlot <me@gauravgahlot.in>
2026-07-15 19:47:03 +0200

    respect the system display setting
    Signed-off-by: Gaurav Gahlot <me@gauravgahlot.in>

--- .config/nvim/lua/config/core/options.lua
+++ .config/nvim/lua/config/core/options.lua
@@ -6,9 +6,9 @@ opt.number = true
 opt.relativenumber = true
 
 -- tabs and indentation
-opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2    -- 2 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -16,7 +16,7 @@ opt.wrap = true
 
 -- search
 opt.ignorecase = true -- ignore case when seaching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 opt.hlsearch = false
 opt.incsearch = true
 
@@ -25,8 +25,9 @@ opt.cursorline = true
 
 -- appearance
 opt.termguicolors = true -- turn on termguicolors for tokyonight colorscheme to work
-opt.background = "dark"  -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"   -- show sign column so that text doesn't shift
+-- opt.background is set dynamically in plugins/colorscheme.lua to follow the
+-- macOS light/dark appearance (matching Ghostty), so it's not hardcoded here.
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace or indent, end of line or insert mode start position
@@ -46,4 +47,4 @@ vim.wo.foldenable = false
 -- others
 opt.iskeyword:append("-")
 opt.scrolloff = 8 -- not working
-opt.colorcolumn = "110"
+-- opt.colorcolumn = "110"
